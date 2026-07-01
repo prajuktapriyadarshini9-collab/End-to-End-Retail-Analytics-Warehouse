063a69d8221990fecec7a18ff76b10a72f2dab7a
santosh20270 <santoshiteram@gmail.com>
2026-07-01 17:25:51 +0530

    Delete metric_clv.sql

--- metric_clv.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    dc.customer_unique_id,
-    SUM(fp.payment_value) AS customer_lifetime_value
-FROM {{ ref('fact_orders') }} fo
-JOIN {{ ref('fact_payments') }} fp
-    ON fo.order_id = fp.order_id
-JOIN {{ ref('dim_customers') }} dc
-    ON fo.customer_id = dc.customer_id
-GROUP BY dc.customer_unique_id
